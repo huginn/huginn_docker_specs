88f8c6bf669d90ef5cc08f4f73069dc0f355981e
Dominik Sander <git@dsander.de>
2023-03-25 14:51:33 +0000

    Make permissions work on linux (requires sudo)

--- spec/support/shared_contexts/with_volume.rb
+++ spec/support/shared_contexts/with_volume.rb
@@ -1,12 +1,16 @@
 RSpec.shared_context 'with volume' do |data_source: nil|
   before(:all) do
-    FileUtils.rm_rf File.join(Dir.pwd, 'spec', 'mysql-data')
+    volume = File.join(Dir.pwd, 'spec', 'mysql-data')
+    `sudo rm -rf #{volume}`
+    FileUtils.mkdir_p volume
     if data_source
-      FileUtils.cp_r File.join(Dir.pwd, data_source), File.join(Dir.pwd, 'spec', 'mysql-data')
+      `sudo cp -r #{File.join(Dir.pwd, data_source)}/* #{volume}`
     end
+    `sudo chown 1001 -R #{volume}`
   end
 
   after(:all) do
-    FileUtils.rm_rf File.join(Dir.pwd, 'spec', 'mysql-data')
+    volume = File.join(Dir.pwd, 'spec', 'mysql-data')
+    `sudo rm -rf #{volume}`
   end
 end
